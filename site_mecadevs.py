6b8b18556c43893d845c81350ccb1859c95dfc94
Orion <140091470+MecaDevs@users.noreply.github.com>
2023-07-29 00:49:46 -0300

    news

--- site_mecadevs.py
+++ site_mecadevs.py
@@ -5,16 +5,15 @@ app.secret_key = "jllor_2005"
 
 @app.route('/')
 def home_page():
-    flash("What's your name?")
     return render_template("mecadevs_homepage.html")
 
 @app.route("/medic_dropper")    
 def medic_dropper_home_page():
     return render_template("medicdropper_homepage.html")
 
-@app.route('/usuarios/<nome_usuario>')
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario) 
+@app.route("/parceiros")
+def parceiros_home_page():
+    return render_template("parceiros_home_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
